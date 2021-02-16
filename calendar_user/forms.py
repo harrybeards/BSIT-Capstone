e6072815edbc9f98684bb32041ff413566cfe613
Judson King <judking@protonmail.ch>
2021-02-16 15:19:41 -0600

    Changed the separate date and time widgets into a single DateTimeInput widget in Calendar

--- calendar_user/forms.py
+++ calendar_user/forms.py
@@ -1,23 +1,18 @@
 from django import forms
 from django.forms import ModelForm
 from .models import Meal
-from .widgets import FengyuanChenDatePickerInput, WickedPicker
+from .widgets import BootstrapDateTimePickerInput
 
 
 class AddMealForm(ModelForm):
-    day = forms.DateField(input_formats=['%d/%m/%Y'],
-                          widget=FengyuanChenDatePickerInput())
-    start_time = forms.TimeInput(input_formats=['%H:%M %p'],
-                                 widget=WickedPicker(), localize=True)
-    end_time = forms.TimeInput(input_formats=['%H:%M %p'],
-                                 widget=WickedPicker())
-
-    # widgets = {
-    #     'day': forms.SelectDateWidget(
-    #         empty_label=("Choose Year", "Choose Month", "Choose Day"),
-    #     )
-    # }
+    date = forms.DateTimeField(
+        input_formats=['%d/%m/%Y %H:%M'],
+        widget=forms.DateTimeInput(attrs={
+            'class': 'form-control datetimepicker-input',
+            'data-target': '#datetimepicker1'
+        })
+    )
 
     class Meta:
         model = Meal
-        fields = ['title', 'day', 'start_time', 'end_time', 'notes']
+        fields = ['title', 'date', 'notes']
